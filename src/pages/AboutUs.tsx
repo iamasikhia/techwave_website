977b5e8148cad228aaedaf30b91b388d70bc80b7
Iseoluwa Asikhia <iseoluwaasikhia01@gmail.com>
2026-04-08 18:08:27 -0400

    new changes

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -108,7 +108,7 @@ export default function AboutUs() {
         </section>
 
         {/* What We Do */}
-        <section className="bg-white px-4 md:px-8 py-8 pb-48">
+        <section className="bg-white px-4 md:px-8 py-8">
           <div className="max-w-5xl mx-auto text-center">
             <StackedWhatWeDo />
           </div>
